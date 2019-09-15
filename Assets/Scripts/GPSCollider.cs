dc49d5b33116b64cb457e08e65138ee21fdf3e36
ColdThor <chrisfodor333@gmail.com>
2019-09-15 19:38:28 +0200

    onClickListener

---
+++ Assets/Scripts/GPSCollider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GPSCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Stationary)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+
+            if (Physics.Raycast(ray, out hit, 100f))
+            {
+                if (hit.transform.tag == "MilleniumItem")
+                {
+                    MilleniumPuzzle item = hit.transform.GetComponent<MilleniumPuzzle>();
+                    SceneManager.LoadScene(8);
+                }
+            }
+        }
+    }
+}
