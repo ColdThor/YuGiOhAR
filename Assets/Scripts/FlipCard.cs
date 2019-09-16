21c1de31a609425e4cf6bf47106f13235896a638
ColdThor <chrisfodor333@gmail.com>
2019-09-16 12:02:18 +0200

    CardFlip

---
+++ Assets/Scripts/FlipCard.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipCard : MonoBehaviour
+{
+
+    public int fps = 60;
+    public float RotateDegreePerSecond = 180f;
+    public bool IsFaceUp = false;
+
+    const float FLIP_LIMIT_DEGREE = 180f;
+
+    float waitTime;
+    bool IsAnimationProcessing = false;
+
+
+    void Start()
+    {
+        waitTime = 1.0f / fps;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnMouseDown()
+    {
+       if(IsAnimationProcessing)
+        {
+            return;
+        }
+        StartCoroutine(flip() );
+    }
+
+    IEnumerator flip()
+    {
+        IsAnimationProcessing = true;
+
+        bool done = false;
+        while(!done)
+        {
+            float degree = RotateDegreePerSecond * Time.deltaTime;
+
+            if(IsFaceUp)
+            {
+                degree = -degree;
+            }
+
+            transform.Rotate(new Vector3(0,degree,0));
+
+            if(FLIP_LIMIT_DEGREE < transform.eulerAngles.y)
+            {
+                transform.Rotate(new Vector3(0, -degree, 0));
+                done = true;
+            }
+
+
+            yield return new WaitForSeconds(waitTime);
+        }
+
+        IsFaceUp = !IsFaceUp;
+        IsAnimationProcessing = false;
+    }
+
+
+
+}
